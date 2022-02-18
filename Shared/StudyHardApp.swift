47c8164094813c659356c7ba0df4b3d3de94ac45
jinmedi <jinmedi@hanmail.net>
2022-02-18 10:23:58 +0900

    Initial Commit

---
+++ Shared/StudyHardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyHardApp.swift
+//  Shared
+//
+//  Created by 김종엽 on 2022/02/18.
+//
+
+import SwiftUI
+
+@main
+struct StudyHardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
